fa8a25c849fa1a340c498fb259b14bc0b08052b4
carlosgrm1444 <carlosgabrielrm1444@gmail.com>
2025-08-04 16:35:11 -0600

    fetch(main) debug para datos de la bd 15)

--- src/main.ts
+++ src/main.ts
@@ -5,15 +5,15 @@ import * as process from 'process';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  console.log('🌐 ENV desde Railway:', {
-    type: 'mysql',
-    host: process.env.MYSQLHOST,
-    port: parseInt(process.env.MYSQLPORT || '3306', 10),
-    username: process.env.MYSQLUSER,
-    password: process.env.MYSQLPASSWORD,
-    database: process.env.MYSQL_DATABASE,
-    autoLoadEntities: false,
-  });
+  // console.log('🌐 ENV desde Railway:', {
+  //   type: 'mysql',
+  //   host: process.env.MYSQLHOST,
+  //   port: parseInt(process.env.MYSQLPORT || '3306', 10),
+  //   username: process.env.MYSQLUSER,
+  //   password: process.env.MYSQLPASSWORD,
+  //   database: process.env.MYSQL_DATABASE,
+  //   autoLoadEntities: false,
+  // });
 
   //   console.log('🌐 ENV desde Railway:', {
   //   type: 'mysql',
