6b32011e6147dbaa261bc5abbcfc2720311f5d9c
carlosgrm1444 <carlosgabrielrm1444@gmail.com>
2025-08-04 14:32:34 -0600

    fetch(main) debug para datos de la bd 3)

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,16 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+  console.log('🌐 ENV desde Railway:', {
+    type: 'mysql',
+    host: process.env.DB_HOST,
+    port: parseInt(process.env.DB_PORT || '3306', 10),
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    autoLoadEntities: true,
+  });
+
   const app = await NestFactory.create(AppModule);
 
   // ✅ Validaciones globales
@@ -22,16 +32,6 @@ async function bootstrap() {
 
   // ✅ Escucha en el puerto
   await app.listen(process.env.PORT || 3000);
-
-  console.log('🌐 ENV desde Railway:', {
-    type: 'mysql',
-    host: process.env.DB_HOST,
-    port: parseInt(process.env.DB_PORT || '3306', 10),
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    autoLoadEntities: true,
-  });
 }
 // eslint-disable-next-line @typescript-eslint/no-floating-promises
 bootstrap();
